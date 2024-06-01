53bc2c1b37f581aa7499ac5fb76fe4443f9563a6
Chien Tan Kieu (Ryan) <kennyjoe1311@gmail.com>
2024-06-01 18:07:07 +0700

    remove redundants

--- web-app/src/models/Group.ts
+++ web-app/src/models/Group.ts
@@ -1,5 +1,3 @@
-import * as yup from 'yup';
-
 type Group = {
   _id: string;
   name: string;
@@ -9,20 +7,3 @@ type Group = {
 export default Group;
 export type GroupResponse = Group;
 export type GroupRequest = Omit<Group, '_id'>;
-export const GroupForm = {
-  defaultValues: {
-    name: undefined,
-    description: undefined,
-  },
-  validationSchema: yup
-    .object<GroupRequest>()
-    .shape({
-      name: yup
-        .string()
-        .min(3, 'Group name must be at least 3 characters length')
-        .max(10, 'Group name must be at most 3 characters length')
-        .required('Group name is required'),
-      description: yup.string().required('Group description is required'),
-    })
-    .required(),
-};
